fa9d9f85ab975893b0d86c56e6db7efb52a8fe5e
jack.harrington9 <jack.harrington9@600900B211DE408>
2018-02-22 11:52:40 +1100

    Created Going Home game

---
+++ Assets/~GoingHome/Scripts/Death.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GoingHome
+{
+    public class Death : MonoBehaviour
+    {
+        void Died()
+        {
+            print("the" + name + " has Died");
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.name == "Deathzone" || other.name == "Enemy")
+            {
+                Died();
+            }
+        }
+    }
+}
